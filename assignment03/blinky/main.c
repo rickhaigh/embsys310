5f9cc298a7756cc9dce60cac02d34f7571b55998
rick haigh <woodworker@gmail.com>
2020-10-27 22:06:40 -0700

    separated out blinky and blinky_xor

---
+++ assignment03/blinky/main.c
@@ -0,0 +1,31 @@
+
+#define RCC_BASE 0x40021000
+#define GPIOA_BASE 0x48000000
+#define RCC_AHB2ENR (*((unsigned int*)(RCC_BASE + 0x4C)))
+#define GPIOA_MODER (*((unsigned int*)(GPIOA_BASE + 0x00)))
+#define GPIOA_ODR (*((unsigned int*)(GPIOA_BASE + 0x14)))
+
+#define BIT5 (1<<5)
+
+int main()
+{
+    volatile int counter;
+    
+    RCC_AHB2ENR |= 0x1; // RCC_AHB2ENR: Enable clock to GPIOA
+
+    GPIOA_MODER = 0xABFFF7FF; // GPIOA_MODER: Set GPIOA 5 to output
+    
+    while(1) {    
+                counter = 0;
+        while (counter < 100000) {
+            counter++;
+        }
+        GPIOA_ODR |= BIT5; // GPIOA_ODR: turn on LED1 (PA5) on (set bit 5)
+        
+        counter = 0;
+        while (counter < 100000) {
+            counter++;
+        }
+        GPIOA_ODR &= ~BIT5; // GPIOA_ODR: turn off LED1 (PA5) (clear bit 5)
+    }
+}
