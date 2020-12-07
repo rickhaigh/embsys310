543cccfb7968ed1a51774a00f49d6f8770ba5f4f
rick haigh <woodworker@gmail.com>
2020-12-07 13:10:17 -0800

    updated c and h files

--- assignment07/delay.c
+++ assignment07/delay.c
@@ -1,9 +1,12 @@
 #include "delay.h"
 
 // Delay ms - delays for num_ms number of milliseconds
+// NOTE: Remember that SysTick timer does not run during debugging when the processor is halted
+
 void delay_ms (uint32_t num_ms) {
     
-    g_ms_tick = 0;  // reset count to start new delay
-    while (g_ms_tick < num_ms);
-
+    g_ms_tick = 0;             // reset count to start new delay
+    // SysTick->CTRL = 0x7;    // Enable systick timer
+    while (g_ms_tick < num_ms);// Wait for num_ms to run out
+    // SysTick->CTRL = 0x0;    // disable systick timer
 }
--- assignment07/main.c
+++ assignment07/main.c
@@ -1,6 +1,6 @@
+#include "delay.h"
 #include "stm32l475xx.h"
 #include "stm32l4xx_it.h"
-#include "delay.h"
 
 #define SYS_CLOCK_HZ 4000000u   // Default clock after startup
                                 // Section 3.11 in STM32L475 datasheet DS10969    
@@ -18,6 +18,7 @@ void main()
     GPIOB->MODER |= GPIO_MODER_MODE14_0;  // Set [28] bit
  
     SysTick_Initialize();
+    
     while(1) 
     {
         GPIOB->ODR ^= GPIO_ODR_OD14; // Toggle bit 14
@@ -28,15 +29,16 @@ void main()
 // Function to initialize and enable SysTick
 void SysTick_Initialize(void) 
 {
-    SysTick->LOAD = (SYS_CLOCK_HZ-1)/1000;  // 1ms count down
-    SysTick->VAL = 0x0;
-    SysTick->CTRL = 0x7;
-    
+    SysTick->LOAD = (SYS_CLOCK_HZ-1)/1000;  // setup 1ms count down
+    SysTick->VAL = 0x0;                     // count to 0
+    SysTick->CTRL = 0x7;                    // Select core clock, enable interrupt and enable timer
+                                            // It might make sense to only enable SysTick timer when 
+                                            // it is needed, like in the delay function
 }
 
 void SysTick_Handler(void) 
 {
-    __disable_irq();
-    g_ms_tick++;
-    __enable_irq();
+    __disable_irq();    // Disable interrupts
+    g_ms_tick++;        // Increment global ms tick counter
+    __enable_irq();     // Enable interrupts
 }
